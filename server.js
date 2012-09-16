1479156143ae5500821a2b98292a6ec7f4c27cc7
Ian Murray <ianmurrays@gmail.com>
2012-09-16 11:47:58 -0700

    Adds server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+// @link https://raw.github.com/gist/701407/6db19d14ba4e731a3caf5f634079c43bc4d663fe/static_server.js
+// Don't you dare touch this file bitch!
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8888;
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), "public", uri);
+  
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+// console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
